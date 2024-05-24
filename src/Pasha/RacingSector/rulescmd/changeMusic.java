24c5d2ff37eb813ba38b10a133fbfbd86d1c43ec
Emre Cankaya <emrecankaya00@hotmail.com>
2024-05-25 00:22:28 +0200

    init

---
+++ src/Pasha/RacingSector/rulescmd/changeMusic.java
@@ -0,0 +1,19 @@
+package Pasha.RacingSector.rulescmd;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.InteractionDialogAPI;
+import com.fs.starfarer.api.campaign.rules.MemoryAPI;
+import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
+import com.fs.starfarer.api.util.Misc;
+
+import java.util.List;
+import java.util.Map;
+
+public class changeMusic extends BaseCommandPlugin {
+
+    @Override
+    public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
+        Global.getSoundPlayer().playCustomMusic(1,1,"MyMusic",true);
+        return false;
+    }
+}
